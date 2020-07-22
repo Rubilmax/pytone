e2e65bfc63213a0d4b892e46516f699035b357ad
MerlinEgalite <egalite.merlin@gmail.com>
2020-07-22 17:46:22 +0200

    add notif ios

--- pytone/command_line.py
+++ pytone/command_line.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 import threading
+import pync
 
 from playsound import playsound
 
@@ -12,6 +13,14 @@ from .notifications import notify
 def run_python_command():
     os.system("python " + " ".join(sys.argv[1:]))
     soundfile = get_soundfile("nyancat")
+
+    pync.notify('Hello World')
+    print('test')
+    pync.notify('Hello World', title='Python')
+
+    pync.remove_notifications(os.getpid())
+
+    pync.list_notifications(os.getpid())
 
     threading.Thread(target=notify).start()
     playsound(soundfile)
